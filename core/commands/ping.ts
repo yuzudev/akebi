b16f9178c7542aa0210bccc61c6b80ef0a4c6b87
FabrizioCoder <74118796+FabrizioCoder@users.noreply.github.com>
2022-04-04 23:12:23 -0500

    Update ping.ts

--- core/commands/ping.ts
+++ core/commands/ping.ts
@@ -1,7 +1,6 @@
+import { InteractionResponseTypes } from '../../deps/discord.ts';
 import type { Context } from '../../deps/oasis.ts';
 import { claim } from '../../deps/oasis.ts';
-import { None, Some } from '../../deps/monads.ts';
-import { InteractionResponseTypes } from '../../deps/discord.ts';
 
 class Ping {
     readonly data = {
@@ -14,25 +13,23 @@ class Ping {
     }
 
     async run(ctx: Context) {
-        const now = Date.now();
-        // TODO: make it compatible
-        if (ctx.message) {
-            const msg = await ctx.respondWith('Measuring latency...');
-
-            if (msg.isSome()) {
-                const time = Date.now() - now;
-
-                await ctx.bot.contents.helpers.editMessage(
-                    msg.unwrap().channelId,
-                    msg.unwrap().id,
-                    {
-                        content: `Pong! (${time}ms)`,
-                    },
-                );
-            }
-        }
+        const ping = snowflakeToTimestamp(ctx.interaction!.id) - Date.now();
+        await ctx.bot.contents.helpers.sendInteractionResponse(
+            ctx.interaction!.id,
+            ctx.interaction!.token,
+            {
+                type: InteractionResponseTypes.ChannelMessageWithSource,
+                data: {
+                    content: `Pong! (${ping}ms)`,
+                },
+            },
+        );
     }
 }
 export default Ping;
 
 new Ping();
+
+export function snowflakeToTimestamp(id: bigint) {
+    return Number(id / 4194304n + 1420070400000n);
+}

07a86429f7b3e63175cccd6f74ded3fd7773fdd7
Yuzu <yuzuru@programmer.net>
2022-04-05 04:08:41 +0000

    command: avatar

---
+++ core/commands/avatar.ts
@@ -0,0 +1,54 @@
+import type { Context } from '../../deps/oasis.ts';
+import type { BotWithCache, InteractionDataOption } from '../../deps/discord.ts';
+import { Argument, claim } from '../../deps/oasis.ts';
+import { None, type Option, Some } from '../../deps/monads.ts';
+
+class Avatar {
+    readonly data = {
+        name: 'avatar',
+        description: 'Get the avatar of a user',
+    };
+
+    readonly aliases = ['pfp', 'avy', 'icon'];
+
+    @Argument.User('The user to get the avatar of')
+    declare user: unknown;
+
+    private get options(): unknown[] {
+        return [this.user];
+    }
+
+    constructor() {
+        claim(this, this.options, this.aliases);
+    }
+
+    async run(ctx: Context<BotWithCache>) {
+        const user = Some(
+            ctx.options.isSome() && ctx.options.unwrap()[0] !== undefined
+                ? ctx.bot.contents.users.get(BigInt(ctx.options.unwrap()[0].value as string))
+                : ctx.message
+                ? ctx.bot.contents.users.get(ctx.message.authorId)
+                : ctx.interaction
+                ? ctx.interaction.user
+                : undefined,
+        );
+
+        if (user.isNone()) {
+            await ctx.respondWith('Unkown user');
+        }
+
+        const avatar = ctx.bot.contents.helpers.avatarURL(
+            ctx.userId.unwrap(),
+            user.unwrap().discriminator,
+            {
+                size: 2048,
+                avatar: user.unwrap().avatar,
+            },
+        );
+
+        await ctx.respondWith(avatar);
+    }
+}
+export default Avatar;
+
+new Avatar();

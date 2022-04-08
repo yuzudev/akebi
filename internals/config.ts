6e48dbad2a06b340bf95b1de424d4bb2d61b6dca
Yuzu <yuzuru@programmer.net>
2022-04-07 23:47:33 -0500

    patch: remove unnecesary complexity

--- internals/config.ts
+++ internals/config.ts
@@ -1,23 +1,23 @@
 import { parse } from '../deps/toml.ts';
 
-export declare namespace OasisConfig {
-    export interface t extends Record<string, unknown> {
-        config: {
-            ownerId: string;
-            prefix: string;
-            token: string;
-            botId: string;
-            supportGuildId: string;
-            logsChannelId: string;
-            intents: string[];
-            development: boolean;
-        };
-        handler: {
-            rootDirectory?: string;
-            loadDirectories?: string[];
-            temporaryFile?: boolean;
-        };
-    }
+export const PATH = 'oasis.toml';
+
+export interface t extends Record<string, unknown> {
+    config: {
+        ownerId: string;
+        prefix: string;
+        token: string;
+        botId: string;
+        supportGuildId: string;
+        logsChannelId: string;
+        intents: string[];
+        development: boolean;
+    };
+    handler: {
+        rootDirectory?: string;
+        loadDirectories?: string[];
+        temporaryFile?: boolean;
+    };
 }
 
-export const { config, handler } = parse(Deno.readTextFileSync('oasis.toml')) as OasisConfig.t;
+export const { config, handler } = <t> parse(Deno.readTextFileSync(PATH));

d0dbd4d0cd3c2a1f62aa1b9ae9879d5a714dc9a4
Yuzu <yuzuru@programmer.net>
2022-04-12 21:55:29 -0500

    feat: invite link

--- mod.ts
+++ mod.ts
@@ -2,6 +2,19 @@ import { BitwisePermissionFlags, enableCachePlugin, enablePermissionsPlugin } fr
 import { enableMiddleware } from './internals/middleware.ts';
 import { Client } from './internals/client.ts';
 
+// deno-fmt-ignore
+const BASE_PERMISSIONS = 0
+    | BitwisePermissionFlags.VIEW_CHANNEL
+    | BitwisePermissionFlags.SEND_MESSAGES
+    | BitwisePermissionFlags.EMBED_LINKS
+    | BitwisePermissionFlags.ATTACH_FILES
+    | BitwisePermissionFlags.READ_MESSAGE_HISTORY
+    | BitwisePermissionFlags.CHANGE_NICKNAME;
+
+const inviteUrl = (botId: bigint, perms: number) => (
+    `https://discord.com/oauth2/authorize?client_id=${botId}&scope=bot%20applications.commands&permissions=${perms}`
+);
+
 function main() {
     const client = new Client();
 
@@ -14,7 +27,7 @@ function main() {
 
     client.bot.events.ready = (bot, payload, rawPayload) => {
         console.info('Ready! logged as %s with id %d', payload.user.username, bot.id);
-        console.info(inviteUrl(bot.id, BitwisePermissionFlags.ADMINISTRATOR));
+        console.info(inviteUrl(bot.id, BASE_PERMISSIONS));
         ready(bot, payload, rawPayload);
     };
 
@@ -25,7 +38,3 @@ function main() {
 if (import.meta.main) {
     main();
 }
-
-const inviteUrl = (botId: bigint, perms: number): string => (
-    `https://discord.com/oauth2/authorize?client_id=${botId}&scope=bot application.commands&permissions=${perms}`
-);

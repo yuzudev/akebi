642f6c7efd4b618da4aa1633e786b35e0e73ddce
Yuzu <yuzuru@programmer.net>
2022-06-06 18:53:21 -0500

    remove license

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-/**
- * @license
- * Akebi is a lightweight Discord bot that uses the Oasis framework
- * Copyright (C) 2022 Yuzuru (https://github.com/yuzudev)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * */
-
 import { BitwisePermissionFlags, GatewayIntents, type EventHandlers } from "discordeno";
 import { enableCachePlugin } from "discordeno/cache-plugin";
-import { enablePermissionsPlugin } from "discordeno/permissions-plugin";
 import { enableMiddleware } from "./internals/middleware.js";
 import { OasisClient } from "oasis-framework";
 import { enableCommandContext } from "oasis-framework/contrib";
@@ -77,10 +56,8 @@ const client = new Client({
     },
 });
 
-await client
-    .start()
-    .then(enableMiddleware)
-    .then(enableCachePlugin)
-    .then(enablePermissionsPlugin)
-    // todo: fix this
-    .then(bot => (enableCommandContext(config.prefix)(bot), bot));
+await client.start([
+    enableMiddleware,
+    enableCachePlugin,
+    (bot) => { enableCommandContext(config.prefix)(bot); return bot }
+]);
